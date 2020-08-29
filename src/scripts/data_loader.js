32a5f3f61212672aaeffa437208de2768502caff
Septiana Yoga <septianayoga30@gmail.com>
2020-08-29 15:43:39 +0700

    update style and card items

---
+++ src/scripts/data_loader.js
@@ -0,0 +1,25 @@
+const DATA_LOADER = (data) => {
+  let dataReturn = "";
+  console.log(data.length);
+  for(let i = 0; i < data.length; i++) {
+    if(data[i].description.length > 150) {
+      data[i].description = data[i].description.substring(0, 150) + "..."
+    }
+
+    dataReturn += `
+       <article class="card-article">
+       <img class="article-image" src="${data[i].pictureId}" alt="${data[i].name}">
+        <h3 class="article-title">${data[i].name}</h3>
+        <p class="article-desc">${data[i].description}</p>
+        <div class="article-footer">
+          <p class="article-footer-item">City: ${data[i].city}</p>
+          <p class="article-footer-item">Rating: ${data[i].rating}/5</p>
+        </div>
+       </article>
+     `
+  }
+
+  return dataReturn;
+}
+
+export default DATA_LOADER;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,12 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
-console.log('Hello Coders! :)');
+import * as DATA from '../DATA.json';
+import DATA_LOADER from './data_loader.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  let mainContainer = document.getElementById("main-container");
+  let dataInsert = DATA_LOADER(DATA.restaurants);
+  mainContainer.insertAdjacentHTML("beforeend", dataInsert);
+})
