e0252fb1baebf980a44778c5931c2806047c44bd
Septiana Yoga <septianayoga30@gmail.com>
2020-08-31 22:56:44 +0700

    add responsiveness and hamburger menu add

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,15 @@ import * as DATA from '../DATA.json';
 import DATA_LOADER from './data_loader.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  let mainContainer = document.getElementById("main-container");
+  const mainContainer = document.getElementById("main-container");
+  const navigationWrapper = document.getElementById("navigation-wrapper");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
   let dataInsert = DATA_LOADER(DATA.restaurants);
+
   mainContainer.insertAdjacentHTML("beforeend", dataInsert);
-})
+
+  hamburgerMenu.addEventListener("click", function() {
+    console.log("hamburger menu diklik!");
+    navigationWrapper.classList.toggle("open");
+  });
+});
