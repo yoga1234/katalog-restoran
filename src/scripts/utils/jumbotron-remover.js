1f93017ebda7c2fc424a208ae7ebc1a800a1666e
Septiana Yoga <septianayoga30@gmail.com>
2020-10-05 15:57:22 +0700

    updating bug

--- src/scripts/utils/jumbotron-remover.js
+++ src/scripts/utils/jumbotron-remover.js
@@ -1,4 +1,5 @@
 const jumbotronRemover = () => {
+  console.log(window.location.hash)
   const jumbotronElement = document.querySelector('.jumbotron')
   if (window.location.hash === '#home') {
     jumbotronElement.style.display = 'block'
