93039164f5871a09cfa85a330cb5f08004474de7
Septiana Yoga <septianayoga30@gmail.com>
2020-10-05 12:55:42 +0700

    updating offline features

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,14 +1,39 @@
 const cacheHelper = {
   async cachingAppShell (requests) {
-
+    const cache = await this._openCache()
+    cache.addAll(requests)
   },
 
   async deleteOldCache () {
-
+    const cacheNames = await caches.keys()
+    cacheNames
+      .filter((name) => name !== 'Foodstyle-V1')
+      .map((filteredName) => caches.delete(filteredName))
   },
 
   async revalidateCache (request) {
 
+  },
+
+  async _openCache () {
+    return caches.open('Foodstyle-V1')
+  },
+
+  async _fetchRequest (request) {
+    const response = await fetch(request)
+
+    // jika error
+    if (!response || response.status !== 200) {
+      return response
+    }
+
+    await this._addCache(request)
+    return response
+  },
+
+  async _addCache (request) {
+    const cache = await this._openCache()
+    cache.add(request)
   }
 
 }
